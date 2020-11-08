9218f4b7cd231d8af87f10a08cbe2c534bfe6032
Gregory Roman <gregorymichaelroman@gmail.com>
2020-11-07 23:37:25 -0500

    Update resume.

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -8,14 +8,13 @@ const Resume = () => (
   <Panel theme="white">
     <Container>
       <Heading element="h2" text="Background"/>
-      <p className="bio">
-        Hello, my name is Greg Roman and I'm a software engineer based in Jersey City, NJ.  I have a decade's worth of
-        experience building modern, high performing websites and applications for nationally recognized brands.  I specialize in
-        full-stack javascript applications, and also have experience working with PHP, Java and Roku&reg; channel development in Brightscript.  Please feel free
-        to contact me at <a title="Email" href="mailto:gregorymichaelroman@gmail.com">gregorymichaelroman@gmail.com</a> or follow me on Twitter <a href="https://twitter.com/romanwebnet" title="Twitter" target="_blank" rel="noopener noreferrer">@romanwebnet</a>.
+      <p className={styles.bio}>
+        Hello, my name is Greg Roman and I'm a software engineer based in Jersey City, NJ.  I have years of
+        experience building modern, high performing websites and applications for nationally recognized brands. If you are looking for a new website or app for your business, please 
+        contact me at <a title="Email" href="mailto:gregorymichaelroman@gmail.com">gregorymichaelroman@gmail.com</a> or follow me on Twitter <a href="https://twitter.com/romanwebnet" title="Twitter" target="_blank" rel="noopener noreferrer">@romanwebnet</a>.
       </p>
       <Heading element="h2" text="Skills"/>
-      <div className="skills flex-columns">
+      <div className={`${styles.skills} flex-columns`}>
         <div className="column">
           <h3 className="font-bold">Languages</h3>
           <ul>
@@ -59,7 +58,14 @@ const Resume = () => (
         </div>
       </div>
       <Heading element="h2" text="Experience"/>      
-      <div className="experience list">
+      <div className={`${styles.experience} list`}>
+      <div className="list-item">
+          <div className="list-item-left">
+            <h4>Peloton Interactive Inc</h4>
+            <h5>Senior Software Engineer</h5>
+          </div>
+          <div className="h5 list-item-right"><i>2020 - Current</i></div>
+        </div>           
         <div className="list-item">
           <div className="list-item-left">
             <h4>Anheuser-Busch InBev - New York, NY</h4>
@@ -95,7 +101,7 @@ const Resume = () => (
             <h4>AOL/Oath - New York, NY</h4>
             <h5>Senior Software Engineer</h5>
             <p>
-              Front-end engineer for AOL’s research and development team, AOL Alpha.  Responsible for building various web applications for client and server, as well as native applications for Roku&reg;.
+              Front-end engineer for AOL’s research and development team.  Responsible for building various web applications for client and server, as well as native applications for Roku&reg;.
             </p>
           </div>
           <div className="h5 list-item-right"><i>2014 - 2018</i></div>
